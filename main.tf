224cadfef783b10e1be2b977b91c98edd46f5357
ashishpathak721-devops <ashishpathak721@gmail.com>
2026-06-03 00:27:10 +0530

    maincode

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "abc" {
     name = "testing"
     location = "eastus"
 }
+
+resource "azurerm_resource_group" "abcd" {
+    name = "testing1"
+    location = "eastus"
+}

0322ba93ce480738174dd21103a5c68c92abe4ce
ashishpathak721-devops <ashishpathak721@gmail.com>
2026-06-04 23:12:02 +0530

    add

--- main.tf
+++ main.tf
@@ -3,7 +3,14 @@ resource "azurerm_resource_group" "abc" {
     location = "eastus"
 }
 
-resource "azurerm_resource_group" "abcd" {
-    name = "testing1"
-    location = "eastus"
+
+
+resource "azurerm_storage_account" "xyzg" {
+    depends_on = [ azurerm_resource_group.abc ]
+ name = "testingstorage"
+ resource_group_name = "testing"
+ location = "eastus"
+ account_tier = "Standard"
+ account_replication_type = "LRS"
+
 }
